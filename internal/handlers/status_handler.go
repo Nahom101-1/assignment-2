2d56a01e5af18a9638fc3bf845577d5a1ff38b14
fredrawi <fredrikandreas03@gmail.com>
2025-03-20 15:26:23 +0100

    Added functionality for restcountries, openmeteo, currency and uptime to status_handler

--- internal/handlers/status_handler.go
+++ internal/handlers/status_handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"assignment-2/internal/constants"
 	"assignment-2/internal/models"
 	"assignment-2/utils"
 	"net/http"
@@ -8,14 +9,17 @@ import (
 )
 
 var startTime = time.Now()
+var restCountriesTest = constants.RestCountriesAPI + "name/norway"
+var openMeteoTest = constants.OpenMeteoAPI + "forecast?latitude=52.52&longitude=13.41"
+var currencyTest = constants.CurrencyAPI + "nok"
 
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
 	uptime := int(time.Since(startTime).Seconds())
 	response := models.Status{
-		RestCountriesAPI: "200 OK",
-		OpenMeteoAPI:     "200 OK",
-		CurrencyAPI:      "200 OK",
-		NotificationDB:   "200 OK",
+		RestCountriesAPI: utils.CheckAPIStatus(restCountriesTest),
+		OpenMeteoAPI:     utils.CheckAPIStatus(openMeteoTest),
+		CurrencyAPI:      utils.CheckAPIStatus(currencyTest),
+		NotificationDB:   200,
 		Webhooks:         6,
 		Version:          "v1",
 		Uptime:           uptime,
