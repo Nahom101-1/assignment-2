03a4fa32e53f859f6ce94962eb9664ea1ca83e26
fredrawi <fredrikandreas03@gmail.com>
2025-03-20 15:09:40 +0100

    Added example code to status_handler

--- internal/handlers/status_handler.go
+++ internal/handlers/status_handler.go
@@ -1,10 +1,25 @@
 package handlers
 
 import (
+	"assignment-2/internal/models"
 	"assignment-2/utils"
 	"net/http"
+	"time"
 )
 
+var startTime = time.Now()
+
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
-	utils.JsonResponse(w, "status handler")
+	uptime := int(time.Since(startTime).Seconds())
+	response := models.Status{
+		RestCountriesAPI: "200 OK",
+		OpenMeteoAPI:     "200 OK",
+		CurrencyAPI:      "200 OK",
+		NotificationDB:   "200 OK",
+		Webhooks:         6,
+		Version:          "v1",
+		Uptime:           uptime,
+	}
+
+	utils.JsonResponse(w, response)
 }
