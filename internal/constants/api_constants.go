f96e2160de25561e0b97467b1f457acf74d4d6ef
Nahom <nahomberhane101@gmail.com>
2025-03-31 08:17:23 +0200

    cleaned up constants

--- internal/constants/api_constants.go
+++ internal/constants/api_constants.go
@@ -1,5 +1,7 @@
 package constants
 
-const RestCountriesAPI = "http://129.241.150.113:8080/v3.1/"
-const OpenMeteoAPI = "https://api.open-meteo.com/v1/"
-const CurrencyAPI = "http://129.241.150.113:9090/currency/"
+const (
+	RestCountriesAPI = "http://129.241.150.113:8080/v3.1/"
+	OpenMeteoAPI     = "https://api.open-meteo.com/v1/"
+	CurrencyAPI      = "http://129.241.150.113:9090/currency/"
+)
---
+++ internal/constants/endpointUrls.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	BasePath              = "/"
+	APIVersionPrefix      = "/dashboard/v1"
+	RegistrationsEndpoint = APIVersionPrefix + "/registrations/"
+	DashboardsEndpoint    = APIVersionPrefix + "/dashboards/"
+	NotificationsEndpoint = APIVersionPrefix + "/notifications/"
+	StatusEndpoint        = APIVersionPrefix + "/status/"
+)
