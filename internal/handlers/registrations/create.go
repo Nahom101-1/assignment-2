91e59051d61fe29a3f69120e45b6394493667c66
Nahom <nahomberhane101@gmail.com>
2025-03-31 10:39:10 +0200

    Created registerion(POST) creating a new dashboard config and creating id and timestamp and adding it to the firestore colletion.

--- internal/handlers/registrations/create.go
+++ internal/handlers/registrations/create.go
@@ -3,13 +3,16 @@ package registrations
 import (
 	"encoding/json"
 	"github.com/Nahom101-1/assignment-2/internal/models"
+	"github.com/Nahom101-1/assignment-2/internal/storage"
 	"github.com/Nahom101-1/assignment-2/utils"
 	"log"
 	"net/http"
 )
 
+const Collection = "registrations"
+
 func HandlePostRegistration(w http.ResponseWriter, r *http.Request) {
-	log.Println("Received request:", r.Method, r.URL.Path)
+	log.Printf("POST /registrations received: %s %s\n", r.Method, r.URL.Path)
 
 	var config models.DashboardConfig
 	err := json.NewDecoder(r.Body).Decode(&config)
@@ -18,10 +21,36 @@ func HandlePostRegistration(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Encode and send the JSON response
-	if err := json.NewEncoder(w).Encode(config); err != nil {
-		utils.HandleServiceError(w, err, "(HandleGetRequest(population)) Error encoding response", http.StatusBadRequest)
+	// Generate ID and timestamp
+	id := utils.GenerateID()
+	timestamp := utils.GetTimestamp()
+
+	// Firebase data
+	storedData := map[string]interface{}{
+		"country":    config.Country,
+		"isoCode":    config.IsoCode,
+		"features":   config.Features,
+		"lastChange": timestamp,
+	}
+
+	if _, err := storage.GetClient().Collection(Collection).Doc(id).Set(r.Context(), storedData); err != nil {
+		utils.HandleServiceError(w, err, "Error storing registration in Firestore", http.StatusInternalServerError)
 		return
 	}
 
+	// Prepare Response
+	resp := models.ResponseID{
+		ID:         id,
+		LastChange: timestamp,
+	}
+
+	log.Printf("Registration stored: ID=%s LastChange=%s\n", id, timestamp)
+
+	// Encode and send the JSON response and set header
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		utils.HandleServiceError(w, err, "(HandleGetRequest(population)) Error encoding response", http.StatusBadRequest)
+		return
+	}
 }
