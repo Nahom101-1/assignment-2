be9798ad954e384cc2f3039214727c654bcec6a0
Nahom <nahomberhane101@gmail.com>
2025-04-01 13:53:27 +0200

    Added a global handler for all the methods on the registration endpoint.

--- internal/handlers/registrations/handler.go
+++ internal/handlers/registrations/handler.go
@@ -6,15 +6,27 @@ import (
 	"net/http"
 )
 
-// Handler handles incoming HTTP requests for registration creation
+const Collection = "registrations"
+
+// Handler handles incoming HTTP requests for registration post,get,head,put,delete
 func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received request:", r.Method, r.URL.Path)
 	switch r.Method {
 	case http.MethodPost:
 		HandlePostRegistration(w, r)
+	case http.MethodGet:
+		HandleGetRegistration(w, r)
+	case http.MethodHead:
+		HandleHeadRegistrations(w, r)
+	case http.MethodPut:
+		HandlePutRegistration(w, r)
+	case http.MethodPatch:
+		HandlePatchRegistration(w, r)
+	case http.MethodDelete:
+		HandleDeleteRegistration(w, r)
 	default:
 		http.Error(w,
-			fmt.Sprintf(`{"error": "REST Method '%s' not supported. Supported methods: '%s'."}`,
+			fmt.Sprintf(`{"error": "REST Method '%s' not supported. Supported methods: '%s',"}`,
 				r.Method, http.MethodGet),
 			http.StatusMethodNotAllowed)
 	}
