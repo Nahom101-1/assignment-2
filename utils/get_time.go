557b5e87daf5e001af4bc0e78432a273a5af93c0
Nahom <nahomberhane101@gmail.com>
2025-04-06 23:29:32 +0200

    Add support for different time zones when generating timestamps

--- utils/get_time.go
+++ utils/get_time.go
@@ -6,5 +6,5 @@ import (
 
 // GetTimestamp returns the current time in "YYYYMMDD HH:MM" format
 func GetTimestamp() string {
-	return time.Now().Format("20060102 15:04")
+	return time.Now().UTC().Format(time.RFC3339)
 }
