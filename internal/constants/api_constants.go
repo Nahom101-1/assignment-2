dfb93a75887a29927d2cd1fea0652450b92acf06
TimHarseth <timhar@ntnu.no>
2025-04-08 21:52:50 +0200

    Changes to Api url

--- internal/constants/api_constants.go
+++ internal/constants/api_constants.go
@@ -1,7 +1,6 @@
 package constants
 
-const (
-	RestCountriesAPI = "http://129.241.150.113:8080/v3.1/name/"
-	OpenMeteoAPI     = "https://api.open-meteo.com/v1/forecast"
-	CurrencyAPI      = "http://129.241.150.113:9090/currency/"
-)
+var RestCountriesAPI = "http://129.241.150.113:8080/v3.1/name/"
+var RestCountriesAPI_2 = "http://129.241.150.113:8080/v3.1/name/"
+var OpenMeteoAPI = "https://api.open-meteo.com/v1/forecast"
+var CurrencyAPI = "http://129.241.150.113:9090/currency/"
