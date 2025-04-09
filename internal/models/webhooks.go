9d8f88188bd45eac925bcc21b19eb83d5ceda235
TimHarseth <timhar@ntnu.no>
2025-04-09 02:58:33 +0200

    added omitempty to Webhook struct for test functions

--- internal/models/webhooks.go
+++ internal/models/webhooks.go
@@ -2,7 +2,7 @@ package models
 
 // Webhook struct for Registration of a webhook
 type Webhook struct {
-	ID      string `json:"id"`
+	ID      string `json:"id,omitempty"`
 	URL     string `json:"url"`               // URL to be invoked when event occurs
 	Country string `json:"country,omitempty"` // Country that is registered, or empty if all countries
 	Event   string `json:"event"`             // Event on which it is invoked
